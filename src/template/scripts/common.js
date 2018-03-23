6149737370b13bcb276e0715d7fbc1acc3c05f08
e5ash <io@e5ash.com>
2018-03-23 13:17:46 +0300

    100%

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -2,4 +2,48 @@ $(document).ready(function($) {
 
 	$('.text_phone').mask('+7 (999) 999-99-99');
 	
+
+	$('.gallery__slider').slick({
+		centerMode: true,
+		variableWidth: true,
+		dots: true,
+		responsive: [{
+			breakpoint: 815,
+			 settings: {
+        centerMode: false,
+				variableWidth: false,
+      }
+		}]
+	})
+
+	$('.program__slider-wrap').slick({
+		dots: true
+	})
+
+	$('.program__btn-more').click(function(event) {
+		$('.program__item').slideDown(300)
+		$(this).hide();
+		$('.program__slider').slick('reinit');
+	});
+
+	$('.reviews__list').slick({
+		slidesToShow: 2,
+		responsive: [{
+			breakpoint: 992,
+			 settings: {
+        slidesToShow: 1
+      }
+		}]
+	})
+
+	$(".header__nav-wrap").on("click","a", function (event) {
+		event.preventDefault();
+
+		var id  = $(this).attr('href'),
+
+			top = $(id).offset().top;
+		
+		$('body,html').animate({scrollTop: top - 30}, 1500);
+	});
+
 });
